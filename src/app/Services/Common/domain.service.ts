0fb52f69444014fd490621b48f6b289df7de64d7
Osman KUŞTU <64109752+osmankustu@users.noreply.github.com>
2024-02-29 18:10:21 +0300

    development

--- src/app/Services/Common/domain.service.ts
+++ src/app/Services/Common/domain.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DomainService {
-//productionDomain:string="https://antalyasmartotomasyon.somee.com/Data/api/";
+
   //testDomain:string="";
   productionDomain:string="https://localhost:7115/api/";
   constructor() { }
